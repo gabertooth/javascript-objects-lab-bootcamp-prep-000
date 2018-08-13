08b79792c717f4a4359f40325d6b7873997b08af
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-13 15:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},object,{[key]:value});
 }
-functiondestructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
+}
 
   
